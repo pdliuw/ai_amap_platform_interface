b5498154dae380938bcc014faa349322723d76f2
Air <xkrossiapd163@163.com>
2021-04-01 11:48:29 +0800

    * add location service interface

--- lib/src/ai_amap_global_config.dart
+++ lib/src/ai_amap_global_config.dart
@@ -3,13 +3,15 @@
 class AiAMapGlobalConfig {
   /// ViewTypeId
   static const String VIEW_TYPE_ID_MAP_LOCATION_PLATFORM_VIEW =
-      "view_type_id_map_location_platform_view";
+      "ai_amap:view_type_id_map_location_platform_view";
   static const String VIEW_TYPE_ID_MAP_PLATFORM_VIEW =
-      "view_type_id_map_platform_view";
+      "ai_amap:view_type_id_map_platform_view";
 
   /// MethodChannelId
   static const String METHOD_CHANNEL_ID_MAP_PLATFORM_VIEW =
-      "method_channel_id_map_platform_view";
+      "ai_amap:method_channel_id_map_platform_view";
   static const String METHOD_CHANNEL_ID_MAP_LOCATION_PLATFORM_VIEW =
-      "method_channel_id_map_location_platform_view";
+      "ai_amap:method_channel_id_map_location_platform_view";
+  static const String METHOD_CHANNEL_ID_MAP_LOCATION =
+      "ai_amap:method_channel_id_map_location";
 }
